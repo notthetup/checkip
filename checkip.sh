ddb903f90c707a66c6d4ef069fd497e9a68fff9a
chinmay-archbox <notthetup@gmail.com>
2012-12-27 23:53:01 +0800

    Added some robust ip validation detection and repeated checking

--- checkip.sh
+++ checkip.sh
@@ -1,6 +1,11 @@
 histlogfile="$HOME/.checkip/iphist.log"
+ValidIpAddressRegex="^([0-9]{1,3}\.){3}[0-9]{1,3}$";
+count=0;
 
-ipaddr=$(curl -s http://ipecho.net/plain )
+until [[ ("$ipaddr" =~ $ValidIpAddressRegex) || ($count > 5) ]] ; do
+    ipaddr=$(curl -s http://ipecho.net/plain)
+    count=$[count+1];
+done
 
 if [ -f $histlogfile ]; then
     lastip=$(tail -n 1 $histlogfile)
@@ -8,12 +13,17 @@ else
     lastip="0"
 fi
 
-if [ "$lastip" == "" ]; then
-    	date +%c >> $histlogfile
-    	echo "No DNS :(" >> $histlogfile
-else
+
+if [[ "$ipaddr" =~ $ValidIpAddressRegex ]] ; then
     if  [ "$lastip" != "$ipaddr" ]; then
     	date +%c >> $histlogfile
     	echo $ipaddr >> $histlogfile
+    else
+    	echo -ne "-" >> $histlogfile
+	date +%c >> $histlogfile
+    	echo $ipaddr >> $histlogfile
     fi
+else
+    	date +%c >> $histlogfile
+    	echo "No DNS :(" >> $histlogfile
 fi
